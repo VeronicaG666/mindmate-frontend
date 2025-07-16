1e37c9faeb49af218e5516e34ffb40b07c6ebcbb
VeronicaG666 <varnikaigdtuw@gmail.com>
2025-07-17 01:18:55 +0530

    Fixed emotion aliasing for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
     proxy: {
       '/api': 'http://127.0.0.1:5000'
     }
+  },
+  resolve: {
+    alias: {
+      '@emotion/react': path.resolve(__dirname, 'node_modules/@emotion/react'),
+    },
   }
 })
